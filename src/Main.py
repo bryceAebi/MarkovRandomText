0f95f0033c2e9ad778791f2c5ced17993fa4bf3b
bryce_aebi@brown.edu <bryce@Bryce-Aebis-MacBook-Pro.local>
2013-05-25 14:33:01 -0700

    adding support for higher order markov chains

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,7 @@
-'''
+''' Markov Text Generator
 Created on May 24, 2013
+
+Provided 1 or more training documents, this 
 
 @author: bryce
 '''
@@ -28,46 +30,46 @@ def process_file(file_name, chunk_size):
                 if not line:
                     return
                 line_chunks.extend(line.split())
-            following_words = line_chunks[1:chunk_size+1]
-            assert len(following_words) == chunk_size
-            update_bigram_counts(line_chunks[0], tuple(following_words))
+            prev_words = line_chunks[0:chunk_size]
+            assert len(prev_words) == chunk_size
+            update_bigram_counts(tuple(prev_words), line_chunks[chunk_size])
             line_chunks = line_chunks[1:]
 
 
-def update_bigram_counts(word, following_words):
+def update_bigram_counts(prev_words, word):
     '''Record the occurrence of following_word after word in a global dictionary bigram_counts.'''
-    if bigram_counts.has_key(word):
-        word_count_map = bigram_counts[word]
-        count = word_count_map.get(following_words,0)
-        word_count_map[following_words] = count+1
+    if bigram_counts.has_key(prev_words):
+        word_count_map = bigram_counts[prev_words]
+        count = word_count_map.get(word,0)
+        word_count_map[word] = count+1
     else:
-        bigram_counts[word] = {following_words: 1}
+        bigram_counts[prev_words] = {word: 1}
 
 
 def calculate_probabilities(bigrams,probabilities):
     '''For each word in the corpus, determines the probabilities of the next possible words. Stores results in probabilities.'''
-    for (word, following_words_map) in bigrams.items():
-        total_words = sum(following_words_map.values())
-        probabilities[word] = {}
-        for (w,count) in following_words_map.items():
-            probabilities[word][w] = float(count)/total_words
+    for (prev_words, following_word_map) in bigrams.items():
+        total_words = sum(following_word_map.values())
+        probabilities[prev_words] = {}
+        for (w,count) in following_word_map.items():
+            probabilities[prev_words][w] = float(count)/total_words
 
 
 def produce_text(output_file, output_size, probabilities):
     '''Using the calculated word sequence probabilities, generates random text with output_size words and writes it to output_file.'''
     with open (output_file,'w') as output:
         rand_index = random.randint(0, len(probabilities.keys())-1)
-        first_word = probabilities.keys()[rand_index]
+        first_words = probabilities.keys()[rand_index]
         counter = 0
-        to_print = first_word
-        last_word = first_word
-        print output_size
+        to_print = ''
+        last_words = first_words
         for _ in range(output_size):
-            next_words = choose_next_words(last_word, probabilities)
-            last_word = next_words[len(next_words)-1]
+            next_word = choose_next_word(last_words, probabilities)
+            temp = list(last_words)[1:]
+            temp.append(next_word)
+            last_words = tuple(temp)
             counter += 1
-            for word in next_words:
-                to_print += (' '+ word)
+            to_print += (' '+ next_word)
             if counter > 100:
                 output.write(to_print)
                 to_print = ''
@@ -75,19 +77,19 @@ def produce_text(output_file, output_size, probabilities):
         output.write(to_print)
 
 
-def choose_next_words(last_word, probabilities):
-    next_words_map = probabilities[last_word]
+def choose_next_word(last_words, probabilities):
+    next_word_map = probabilities[last_words]
     rand = random.random()
     prob_sum = 0.0
-    final_words = []
-    for (next_words, prob) in next_words_map.items():
-        final_words = list(next_words)
+    final_word = ''
+    for (next_word, prob) in next_word_map.items():
+        final_word = next_word
         prob_sum += prob
         if rand <= prob_sum:
-            return list(next_words)
+            return next_word
 
     assert prob_sum <= 1
-    return final_words
+    return final_word
 
 
 def main():
